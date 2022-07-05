9c9390856825b58a15f0cacc481bf3f15784b838
Shifaul Islam <khanshifaul@gmail.com>
2022-07-05 09:27:14 +0600

    Commit

--- Progress/js/site.js
+++ Progress/js/site.js
@@ -5,22 +5,122 @@ var ball = document.querySelector('.darkmode-ball')
 
 // Adding an eventListener function to change color everytime var check is changed.(checked & unchecked)
 
-check.addEventListener('change',function(){
+check.addEventListener('change', function () {
 
   //   conditions to apply when checkbox is checked
 
-  if(this.checked == true){
+  if (this.checked == true) {
     darkmode.setDarkMode(true)
-    box.setAttribute('style','background-color:white;')
-    ball.setAttribute('style','transform:translatex(100%);')
+    box.setAttribute('style', 'background-color:white;')
+    ball.setAttribute('style', 'transform:translatex(100%);')
   }
 
   //   conditions to apply when checkbox is unchecked
 
-  if(this.checked == false){
+  if (this.checked == false) {
     darkmode.setDarkMode(false)
-    box.setAttribute('style','background-color:black; color:white;')
-    ball.setAttribute('style','transform:translatex(0%);')
+    box.setAttribute('style', 'background-color:black; color:white;')
+    ball.setAttribute('style', 'transform:translatex(0%);')
   }
 })
+// ************************ Drag and drop ***************** //
+let dropArea = document.getElementById("drop-area")
 
+  // Prevent default drag behaviors
+  ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+    document.body.addEventListener(eventName, preventDefaults, false)
+  })
+
+  // Highlight drop area when item is dragged over it
+  ;['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+  })
+
+  ;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+  })
+
+// Handle dropped files
+dropArea.addEventListener('drop', handleDrop, false)
+
+function preventDefaults(e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('active')
+}
+
+function handleDrop(e) {
+  var dt = e.dataTransfer
+  var files = dt.files
+
+  handleFiles(files)
+}
+
+let uploadProgress = []
+let progressBar = document.getElementById('progress-bar')
+
+function initializeProgress(numFiles) {
+  progressBar.value = 0
+  uploadProgress = []
+
+  for (let i = numFiles; i > 0; i--) {
+    uploadProgress.push(0)
+  }
+}
+
+function updateProgress(fileNumber, percent) {
+  uploadProgress[fileNumber] = percent
+  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
+  progressBar.value = total
+}
+
+function handleFiles(files) {
+  files = [...files]
+  initializeProgress(files.length)
+  files.forEach(uploadFile)
+  files.forEach(previewFile)
+}
+
+function previewFile(file) {
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function () {
+    let img = document.createElement('img')
+    img.src = reader.result
+    document.getElementById('gallery').appendChild(img)
+  }
+}
+
+function uploadFile(file, i) {
+  var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
+  var xhr = new XMLHttpRequest()
+  var formData = new FormData()
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+
+  // Update progress (can be used to show progress indicator)
+  xhr.upload.addEventListener("progress", function (e) {
+    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
+  })
+
+  xhr.addEventListener('readystatechange', function (e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      updateProgress(i, 100) // <- Add this
+    }
+    else if (xhr.readyState == 4 && xhr.status != 200) {
+      // Error. Inform the user
+    }
+  })
+
+  formData.append('upload_preset', 'ujpu6gyk')
+  formData.append('file', file)
+  xhr.send(formData)
+}
